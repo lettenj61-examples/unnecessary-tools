6273fa2d6f5026a331227dec6dce28e4414cb9af
lettenj61 <letitscalable@gmail.com>
2022-01-12 02:11:58 +0900

    `create-next-app`

--- src/app.tsx
+++
@@ -1,5 +0,0 @@
-import Nano from "nano-jsx";
-
-const Greet = (props: { name: string }): Element => <h1>Hello, {props.name}</h1>;
-
-Nano.render(<Greet name="you" />, document.getElementById("app"));
