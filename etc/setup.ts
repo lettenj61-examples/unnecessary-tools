0bac8457c1bbf88711c882fa1835903c79485a05
lettenj61 <letitscalable@gmail.com>
2022-01-21 06:58:13 +0900

    fix: Support all advanced date formats
    * Add dependent Day.js plugins

--- etc/setup.ts
+++ etc/setup.ts
@@ -2,15 +2,23 @@ import dayjs from 'dayjs'
 import advancedFormat from 'dayjs/plugin/advancedFormat'
 import dayOfYear from 'dayjs/plugin/dayOfYear'
 import duration from 'dayjs/plugin/duration'
+import isoWeek from 'dayjs/plugin/isoWeek'
 import quarterOfYear from 'dayjs/plugin/quarterOfYear'
 import relativeTime from 'dayjs/plugin/relativeTime'
+import timezone from 'dayjs/plugin/timezone'
+import weekOfYear from 'dayjs/plugin/weekOfYear'
+import weekYear from 'dayjs/plugin/weekYear'
 
 const plugins = [
   advancedFormat,
   dayOfYear,
   duration,
+  isoWeek,
   quarterOfYear,
   relativeTime,
+  timezone,
+  weekOfYear,
+  weekYear,
 ]
 
 for (const p of plugins) {
