8c118b3ddb47c6bf04a0789bff40bacae2caceab
lettenj61 <letitscalable@gmail.com>
2022-02-12 04:42:37 +0900

    chore: Fix script for GTM

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Script
+        id="googletagmanager"
         strategy='afterInteractive'
         dangerouslySetInnerHTML={{
           __html: `
