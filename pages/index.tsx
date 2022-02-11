fae1b429d6e8beb1e2bddf1859d1799de847a799
lettenj61 <letitscalable@gmail.com>
2022-02-12 05:05:06 +0900

    chore: Add site verification

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>Top | Unnecessary Tools</title>
+        <meta name="google-site-verification" content="1f3kQIUs0-aeaiK6woBClf0z2iNJLp0mn_T2XDxwiFY" />
       </Head>
       <section className="hero is-primary is-fullheight">
         <div className="hero-body">
