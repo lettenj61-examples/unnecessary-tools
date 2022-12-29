c1a3424a1441210172f7bc2fd6c58073749e08fd
lettenj61 <letitscalable@gmail.com>
2022-12-29 16:05:27 +0900

    chore: Add document title in CSV page

--- pages/csv.tsx
+++ pages/csv.tsx
@@ -1,9 +1,14 @@
+import Head from 'next/head'
 import Csv from '../components/csv'
 import TableCleaner from '../components/tableCleaner'
 
 export default function Page() {
   return (
     <>
+      <Head>
+        <title>CSV Utilities | Unnecessary Tools</title>
+      </Head>
+
       <section className="hero is-small">
         <div className="hero-body">
           <p className="title">Handle CSV</p>
